21a683d818408be5e7418eeb582a44d168ced1dc
Qi <i@yangqi.co>
2025-07-22 16:37:16 +0800

    包配置

---
+++ src/ConfigProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XHyperf\LoggerPlus;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use XHyperf\LoggerPlus\Guzzle\GuzzleLogAspect;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                StdoutLoggerInterface::class => StdoutLoggerFactory::class,
+            ],
+            'listeners'    => [
+            ],
+            'annotations'  => [
+                'scan' => [
+                    'collectors' => [
+                    ],
+                ],
+            ],
+            'aspects'      => [
+                GuzzleLogAspect::class,
+            ],
+            'publish'      => [
+                [
+                    'id'          => 'config',
+                    'description' => 'The config for logger.',
+                    'source'      => __DIR__ . '/../publish/logger.php',
+                    'destination' => BASE_PATH . '/config/autoload/logger.php',
+                ],
+            ],
+        ];
+    }
+}
