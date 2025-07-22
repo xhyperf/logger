a15a0852f56263f6eb538554bb84664826d25c9f
Qi <i@yangqi.co>
2025-07-22 16:37:02 +0800

    config key

---
+++ src/ConfigKey.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace xhyperf\LoggerPlus;
+
+class ConfigKey
+{
+    /**
+     * 输出类型
+     */
+    const string OUTPUT_TYPE = 'logger.output.type';
+
+    /**
+     * 是否收集响应正文数据
+     */
+    const string RESPONSE_ENABLE = 'logger.gather.response.enable';
+}
